0a389db19a6ace2f2bdb292e004318ffcdcca77a
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-10 23:05:31 +0400

    Add declarations of main methods and head/tail fields in SinglyLinkedList

--- SinglyLinkedList.cs
+++ SinglyLinkedList.cs
@@ -15,5 +15,15 @@
             public int Value { get => value; set => this.value = value; }
             public Node Next { get => next; set => next = value; }
         }
+
+        private Node head;
+        private Node tail;
+
+        public void Add(int value) { }
+        public bool Contains(int value) { return false; }
+        public bool Remove(int value) { return false; }
+        public void Traverse() { }
+        public void ReverseTraversal() { }
+
     }
 }
