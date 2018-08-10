604d7ce4ef5a8852147df8efd96f1891cf15b213
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-10 22:58:54 +0400

    Add base node class for SinglyLinkedList

--- SinglyLinkedList.cs
+++ SinglyLinkedList.cs
@@ -2,6 +2,18 @@
 {
     class SinglyLinkedList
     {
+        private class Node
+        {
+            private int value;
+            private Node next;
 
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get => value; set => this.value = value; }
+            public Node Next { get => next; set => next = value; }
+        }
     }
 }
