3dd04ebd4969e267e78fc2b242f5d89e510c8618
Razmik Tovmasyan <razmikt@internal.synopsys.com>
2018-08-10 23:13:33 +0400

    Add base node class for DoublyLinkedList

--- DoublyLinkedList.cs
+++ DoublyLinkedList.cs
@@ -2,6 +2,20 @@
 {
     class DoublyLinkedList
     {
+        private class Node
+        {
+            private int value;
+            private Node next;
+            private Node previous;
 
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get => value; set => this.value = value; }
+            public Node Next { get => next; set => next = value; }
+            public Node Previous { get => previous; set => previous = value; }
+        }
     }
 }
